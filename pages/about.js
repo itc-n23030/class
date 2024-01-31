c39751bbe61d648f7fb6b91903187d208601190a
itc-n23030 <n23030@std.it-college.ac.jp>
2024-01-31 13:15:11 +0000

    6.5
    metaデータ追加

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,13 @@ const About = () => {
   const props = { title: 'About', subtitle: 'About development activities' }
   return (
     <Container>
-      <Meta pagetitle='アバウト' />
+      <Meta
+        pageTitle='アバウト'
+        pageDesc='About development activities'
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <Hero {...props} />
       <figure>
         <Image
