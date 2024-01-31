900ba103353a4be6eb99aaafa3b7bb8e8b9fcf9c
itc-n23030 <n23030@std.it-college.ac.jp>
2024-01-31 11:38:21 +0000

    6.4
    サイトの情報を共有できるようにする

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,12 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
+import Meta from 'components/meta'
 
 const Blog = () => {
   const props = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
+      <Meta pageTitle='ブログ' />
       <Hero {...props} />
     </Container>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
+import Meta from 'components/meta'
 const Home = () => {
   const props = { title: 'CUBE', subtitle: 'アウトプットしていくサイト' }
   return (
     <>
       <Container>
+        <Meta />
         <Hero {...props} imageOn />
       </Container>
     </>
