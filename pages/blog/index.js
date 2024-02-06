2bc9ef8dc65504b1b3893b66c7f97286ef157b50
itc-n23030 <n23030@std.it-college.ac.jp>
2024-02-06 01:41:28 +0000

    9.5
    記事一覧ページ

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,28 @@
+import { getAllPosts } from 'lib/api'
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
+import Posts from 'components/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   const props = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero {...props} />
+      <Posts posts={posts} />
     </Container>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Blog
+export { getStaticProps }
