64dfadfb0b1793997a9675750dbaf60d72bcbc70
itc-n23030 <n23030@std.it-college.ac.jp>
2024-02-06 04:49:12 +0000

    9.7
    ホームに記事一覧を掲載

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
-const Home = () => {
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getAllPosts } from 'lib/api'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
+import { getPlaiceholder } from 'plaiceholder'
+
+const Home = ({ posts }) => {
   const props = { title: 'CUBE', subtitle: 'アウトプットしていくサイト' }
   return (
     <>
       <Container>
         <Meta />
         <Hero {...props} imageOn />
+        <Posts posts={posts} />
+        <Pagination nextUrl='/blog' nextText='More Posts' />
       </Container>
     </>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = getImageBuffer(post.eyecatch.url)
+    const { base64 } = getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: posts
+  }
+}
+
+export { getStaticProps }
 export default Home
