cf3e36bbb2ced6311b7d87cda803599f81ee9ed4
itc-n23030 <n23030@std.it-college.ac.jp>
2024-02-06 04:38:41 +0000

    9.6
    eyecatch extra

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
 
 const Blog = ({ posts }) => {
   const props = { title: 'Blog', subtitle: 'Recent Posts' }
@@ -17,6 +20,15 @@ const Blog = ({ posts }) => {
 
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
+
   return {
     props: {
       posts
